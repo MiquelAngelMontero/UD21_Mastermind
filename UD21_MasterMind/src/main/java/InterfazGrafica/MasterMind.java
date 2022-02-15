793dcac096393e2afc80767f286fed873a7ee618
sillyck <ribellas2002@gmail.com>
2022-02-15 11:21:26 +0100

    diseño diveles (beta)

--- UD21_MasterMind/src/main/java/InterfazGrafica/MasterMind.java
+++ UD21_MasterMind/src/main/java/InterfazGrafica/MasterMind.java
@@ -9,13 +9,17 @@ import javax.swing.JSplitPane;
 import javax.swing.BoxLayout;
 import javax.swing.JTable;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.CardLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Color;
 
 public class MasterMind extends JFrame {
 
@@ -39,6 +43,8 @@ public class MasterMind extends JFrame {
 	 * Create the frame.
 	 */
 	public MasterMind() {
+		
+		setTitle("Master Mind");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(30, 30, 475, 471);
 		
@@ -46,14 +52,51 @@ public class MasterMind extends JFrame {
 		
 		JPanel panel2 = new JPanel();
 		
-		JLabel lblNewLabel = new JLabel("New label");
+		JLabel lblNewLabel = new JLabel("Estos son los colores disponibles:  ");
+		
+		JButton rojo = new JButton("");
+		rojo.setIcon(null);
+		rojo.setEnabled(false);
+		rojo.setBackground(Color.RED);
+		
+		JButton naranja = new JButton("");
+		naranja.setForeground(Color.ORANGE);
+		naranja.setEnabled(false);
+		naranja.setBackground(Color.ORANGE);
+		
+		JButton verde = new JButton("");
+		verde.setForeground(new Color(0, 128, 0));
+		verde.setEnabled(false);
+		verde.setBackground(new Color(0, 128, 0));
+		
+		JButton naranja_1 = new JButton("");
+		naranja_1.setForeground(Color.BLUE);
+		naranja_1.setEnabled(false);
+		naranja_1.setBackground(Color.BLUE);
+		
+		JButton naranja_2 = new JButton("");
+		naranja_2.setForeground(Color.PINK);
+		naranja_2.setEnabled(false);
+		naranja_2.setBackground(Color.PINK);
+		
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 439, Short.MAX_VALUE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblNewLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(rojo, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(naranja, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(verde, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(naranja_1, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(naranja_2, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE))
 						.addGroup(groupLayout.createSequentialGroup()
 							.addComponent(panel1, GroupLayout.PREFERRED_SIZE, 316, GroupLayout.PREFERRED_SIZE)
 							.addGap(18)
@@ -63,12 +106,22 @@ public class MasterMind extends JFrame {
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 59, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(panel2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
-						.addComponent(panel1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 59, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(28)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(naranja, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(rojo, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 23, Short.MAX_VALUE)
+								.addComponent(verde, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(naranja_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(naranja_2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel1, GroupLayout.DEFAULT_SIZE, 345, Short.MAX_VALUE)
+						.addComponent(panel2, GroupLayout.DEFAULT_SIZE, 333, Short.MAX_VALUE))
 					.addContainerGap())
 		);
 		getContentPane().setLayout(groupLayout);
