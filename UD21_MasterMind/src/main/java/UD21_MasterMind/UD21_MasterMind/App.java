6cd13d6e0bc9124b693f120443fedaf94638e321
Miquel Angel Montero <mmontero2@institutjaumehuguet.cat>
2022-02-15 09:10:16 +0100

    CommitInicial

---
+++ UD21_MasterMind/src/main/java/UD21_MasterMind/UD21_MasterMind/App.java
@@ -0,0 +1,13 @@
+package UD21_MasterMind.UD21_MasterMind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
