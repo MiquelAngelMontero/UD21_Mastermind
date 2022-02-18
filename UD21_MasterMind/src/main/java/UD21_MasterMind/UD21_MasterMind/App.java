a7ced61916e7c73151f327756afd3934e6b40f8c
Miquel Angel Montero <mmontero2@institutjaumehuguet.cat>
2022-02-18 09:10:26 +0100

    commitFinal

--- UD21_MasterMind/src/main/java/UD21_MasterMind/UD21_MasterMind/App.java
+++ UD21_MasterMind/src/main/java/UD21_MasterMind/UD21_MasterMind/App.java
@@ -11,6 +11,7 @@ public class App
     public static void main( String[] args )
     {
 
+    	//Ejecutamos la interfaz de dificultad
     	InterfazDificultad iD = new InterfazDificultad();
     	
     	int dificultad = iD.getDificultad();
